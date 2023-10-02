6e1045b647356fd04b3293dcc83331d59df2e6bc
ivanfioravanti <ivan.fioravanti@gmail.com>
2023-10-02 22:12:33 +0200

    Removing default System Prompt because some models were failing

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are Chatbot Ollama, a chatbot powered by a large language model. Follow the user's instructions carefully. Respond using markdown.";
+  "";
 
 export const OLLAMA_HOST =
   process.env.OLLAMA_HOST || 'http://127.0.0.1:11434';
