c9d638cc2fcec22eaa3f8a6c477e1db9024fab66
ryansereno <ryansereno@protonmail.com>
2023-10-08 11:55:10 -0400

    added type declaration on getModels(), to resolve type error

--- services/useApiService.ts
+++ services/useApiService.ts
@@ -2,12 +2,14 @@ import { useCallback } from 'react';
 
 import { useFetch } from '@/hooks/useFetch';
 
+import {OllamaModel, OllamaModelDetail} from '@/types/ollama'
+
 
 const useApiService = () => {
   const fetchService = useFetch();
 
   const getModels = useCallback(
-    () => {
+    (): Promise<OllamaModel[]>  => {
       return fetchService.get(`/api/models`, {
         headers: {
           'Content-Type': 'application/json',
