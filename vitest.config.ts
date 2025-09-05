4a9cd68a81a82d32f554db43ee6d7bd92cb7afa7
ivanfioravanti <ivan.fioravanti@gmail.com>
2025-09-05 16:08:11 +0200

    Removing all vulnerabilities and upgrading some packages.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
+    },
+  },
+  test: {
+    coverage: {
+      provider: 'v8',
     },
   },
 });
